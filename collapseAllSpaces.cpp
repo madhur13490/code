917b96569cadd7d245646cc597ba034962bea152
Madhur <madhur13490@gmail.com>
2016-10-19 11:55:22 +0530

    Update collapseAllSpaces.cpp

--- collapseAllSpaces.cpp
+++ collapseAllSpaces.cpp
@@ -6,6 +6,8 @@ using namespace std;
 // With the following code I collapse
 // all subsequent spaces into one.
 // It handles all spaces begging and at the end
+// Need to compile with -std=c++11
+// due to the use of push_back()
 
 int main()
 {
@@ -20,12 +22,14 @@ int main()
                         t = s[i];
                         i++;
                         while (t == s[i]) i++;
+                        // we don't want space at the beginning
                         if (!k.empty())
                                 k.push_back(' ');
                 } else {
                         k.push_back(s[i++]);
                 }
         }
+        // We don't want space at the end.
         if (k[k.length() - 1] == ' ')
                 k.pop_back();
 
