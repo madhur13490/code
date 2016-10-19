ad7a96824809ab0ba3b661efac97e9f3bfdd1c66
Madhur <madhur13490@gmail.com>
2016-10-19 11:53:52 +0530

    Create collapseAllSpaces.cpp

---
+++ collapseAllSpaces.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+// With the following code I collapse
+// all subsequent spaces into one.
+// It handles all spaces begging and at the end
+
+int main()
+{
+        string s;
+        s = "    This   is a                  iio         ";
+        int j = 0;
+        string k;
+        char t;
+
+        for (int i = 0; i < s.length();) {
+                if (s[i] == ' ') {
+                        t = s[i];
+                        i++;
+                        while (t == s[i]) i++;
+                        if (!k.empty())
+                                k.push_back(' ');
+                } else {
+                        k.push_back(s[i++]);
+                }
+        }
+        if (k[k.length() - 1] == ' ')
+                k.pop_back();
+
+        cout << k;
+        return 0;
+}
